83b275e7d52b16822d8bd8dd22a1e7ee4824694a
Askarbek Karasasev <askar.karasaev@gmail.com>
2015-08-18 16:55:22 +0900

    Wired collection into postsList template

--- client/templates/posts/posts_lists.js
+++ client/templates/posts/posts_lists.js
@@ -1,17 +1,5 @@
-var postsData = [
-    {
-        title: 'First blog',
-        body: 'And the very first comment!'
-    },
-    {
-        title: 'Meteor',
-        body: 'Another Javascript framework'
-    },
-    {
-        title: 'Mysterious rib',
-        body: 'Brakes randomly!'
-    }
-];
 Template.postsList.helpers({
-    posts: postsData
+    posts: function() {
+        return Posts.find();
+    }
 });
